96cb43abf638fd7d38565ecaef265ffd8ebe3804
yuetsin <akaza_akari@sjtu.edu.cn>
2020-04-24 19:24:39 +0800

    [add] skeleton

---
+++ instapass-guard/instapass-guard/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  instapass-guard
+//
+//  Created by 法好 on 2020/4/24.
+//  Copyright © 2020 yuetsin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ instapass-guard/instapass-guard/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  instapass-guard
+//
+//  Created by 法好 on 2020/4/24.
+//  Copyright © 2020 yuetsin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
